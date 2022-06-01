0499a2270368c8687bcb3bd4b9bc55568d1525f2
Nathan James <wobaekat@gmail.com>
2022-06-01 17:08:51 +0100

    fix copyright years

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@
 # Cookbook:: kloudspeaker
 # Attributes:: default
 #
-# Copyright:: (C) 2015-2016 Yakara Ltd
+# Copyright:: (C) 2015-2022 Yakara Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -5,7 +5,7 @@
 # Cookbook:: kloudspeaker
 # Recipe:: nginx
 #
-# Copyright:: (C) 2017 Yakara Ltd
+# Copyright:: (C) 2015-2022 Yakara Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
